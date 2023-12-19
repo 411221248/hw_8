a44de24f262ae84a50dec752213d3385ea9880fd
2048io <147537299+411221248@users.noreply.github.com>
2023-12-19 22:48:17 +0800

    Update abc.h

--- abc.h
+++ abc.h
@@ -1,1 +1,17 @@
+#ifndef __ABC_H__
+#define __ABC_H__
 
+#define N 10
+#define EQU(a,b) (a.x==b.x && a.y==b.y)
+
+typedef struct {
+    int x;
+    int y;
+}Axis;
+
+typedef struct {
+    Axis block[N*N];
+    int top;
+}Stack;
+
+#endif
