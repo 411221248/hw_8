3be71f9dd9fc6863b7776296e8e2d090758ab1d7
2048io <147537299+sans2048@users.noreply.github.com>
2023-12-21 22:23:07 +0800

    Update h8.c

--- h8.c
+++ h8.c
@@ -1,6 +1,47 @@
-#include "alo.c"
 #include "abc.h"
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
 
+char maze[N][N]={
+  { '1', '1', '1', '1', '1', '1', '1', '1', '1', '1' },
+  { '0', '0', '0', '1', '1', '0', '1', '1', '0', '1' },
+  { '1', '0', '1', '1', '0', '0', '0', '0', '0', '1' },
+  { '1', '0', '0', '0', '0', '1', '0', '1', '0', '1' },
+  { '1', '1', '1', '0', '1', '1', '0', '1', '0', '1' },
+  { '1', '0', '1', '1', '1', '0','0', '1', '0', '1' },
+  { '1', '0', '0', '0', '0', '0', '1', '1', '0', '0'},
+  { '1', '0', '1', '1', '1', '1', '1', '0', '1', '1' },
+  { '1', '0', '0', '0', '0', '0', '0', '0', '1', '1' },
+  { '1', '1', '1', '1', '1', '1', '1', '1', '1', '1' }
+};
+char mark[N][N];
+char mark2[N][N];
+
+Axis findMove(Stack *s,Axis c){
+  int l=0;
+  for(int dx=-1;dx<2;dx++){
+    for(int dy=-1;dy<2;dy++){
+      if(dx*dy==0&&(dx!=0||dy!=0)){
+        Axis space;
+        space.x=c.x+dx;
+        space.y=c.y+dy;
+        if(space.x>=0&&space.y>=0&&space.x<N&&space.y<N){
+          if(maze[space.x][space.y]=='0' && mark[space.x][space.y]!='*'){
+            mark[space.x][space.y]='*';
+            push(s,space);
+            l=1;
+          }
+        }          
+      }
+    }
+  }
+  if(!l) {
+    mark[c.x][c.y]='0';
+    // printf("%d %d\n",c.x,c.y);
+  }
+  return pop(s);
+}
 int main(){
   Axis start,end,current,noExit={-1,-1};
   Stack stack,stack2;
